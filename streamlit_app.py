a2cde87311f34ff0ad55b9c52c438c08ceabd620
crafty-arl <126638250+crafty-arl@users.noreply.github.com>
2023-04-13 14:55:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,7 @@ from PIL import Image
 # Retrieve the OpenAI Key from the environment variables
 openai.api_key = os.getenv('OPENAI_KEY')
 img_path = "craftytales.png"
-img = Image.open(img_path)
-start_sequence = "\nAI:"
-restart_sequence = "\nHuman: "
+img = Image.open(img_path).resize((500, 500))
 
 # Function to generate story introduction
 @st.cache_data
@@ -31,22 +29,30 @@ def generate_story_intro(character_name, character_race, character_class):
     return response.choices[0].text.strip()
 
 # Character creation interface
-st.title("CraftyTales")
-st.image(img, caption="Image Caption")
-character_name = st.text_input("Enter your character's name:")
-race_options = ["Human", "Elf", "Dwarf", "Orc", "Tiefling"]
-character_race = st.selectbox("Select your character's race:", race_options)
-class_options = ["Warrior", "Mage", "Rogue", "Cleric", "Ranger"]
-character_class = st.selectbox("Select your character's class:", class_options)
+st.set_page_config(layout='wide')
+col1, col2 = st.beta_columns(2)
 
-if st.button("Submit"):
+with col1:
+    st.image(img, caption="A Digital Storytelling Companion")
+
+    st.title("Character Creation")
+    character_name = st.text_input("Enter your character's name:")
+    race_options = ["Human", "Elf", "Dwarf", "Orc", "Tiefling"]
+    character_race = st.selectbox("Select your character's race:", race_options)
+    class_options = ["Warrior", "Mage", "Rogue", "Cleric", "Ranger"]
+    character_class = st.selectbox("Select your character's class:", class_options)
+
+    if st.button("Submit"):
+        if character_name and character_race and character_class:
+            st.subheader("Character Summary")
+            st.write(f"Name: {character_name}")
+            st.write(f"Race: {character_race}")
+            st.write(f"Class: {character_class}")
+
+with col2:
+    st.title("Story Generation")
+
     if character_name and character_race and character_class:
-        st.subheader("Character Summary")
-        st.write(f"Name: {character_name}")
-        st.write(f"Race: {character_race}")
-        st.write(f"Class: {character_class}")
-
-        # Generate and display story introduction
         st.subheader("Story Introduction")
         story_intro = generate_story_intro(character_name, character_race, character_class)
         st.write(story_intro)
