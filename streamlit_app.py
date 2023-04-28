ec702b5cd0f5358c8b2a6e5dfdb7fc87be04363f
crafty-arl <126638250+crafty-arl@users.noreply.github.com>
2023-04-28 16:51:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,95 +1,126 @@
+
+
+# Import required libraries
 import streamlit as st
-import openai
+import yaml
+import pandas as pd
+
+from langchain.agents import (
+    create_json_agent,
+    AgentExecutor
+)
+
+from langchain.agents.agent_toolkits import JsonToolkit
+from langchain.document_loaders import CSVLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+from langchain.requests import TextRequestsWrapper
+from langchain.tools.json.tool import JsonSpec
 import os
-import webbrowser
-import urllib.parse
-from PIL import Image
 
-# Retrieve the OpenAI Key from the environment variables
 openai.api_key = os.getenv('OPENAI_KEY')
-img_path = "craftytales.png"
-img = Image.open(img_path).resize((500, 500))
 
-# Function to generate story introduction
-@st.cache(suppress_st_warning=True)
-def generate_story_intro(character_name, character_race, character_class):
-    prompt = f"A new adventure begins with a character named {character_name}, a {character_race} {character_class}. Introduce the beginning of their story."
+with open("D:\Langchain-Crafty+\Crafty+GameRules") as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+json_spec = JsonSpec(dict_=data, max_value_length=4000)
+json_toolkit = JsonToolkit(spec=json_spec)
 
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
 
-    return response.choices[0].text.strip()
+# Load the documents
+loader = CSVLoader("D:\Langchain-Crafty+\Crafty+Database.csv")
 
-# Function to summarize story in one sentence
-@st.cache(suppress_st_warning=True)
-def summarize_story(story):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Summarize the following story in one sentence:\n\n{story}",
-        temperature=0.3,
-        max_tokens=30,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
+# Create an index
+index_creator = VectorstoreIndexCreator()
+docsearch = index_creator.from_loaders([loader])
 
-    return response.choices[0].text.strip()
+# QA Chain and Agent
+chain = RetrievalQA.from_chain_type(llm=ChatOpenAI(temperature=0,model_name='gpt-3.5-turbo'), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+agent = create_csv_agent(OpenAI(temperature=0,model_name='gpt-3.5-turbo'), 'D:\Langchain-Crafty+\Crafty+Database.csv', verbose=True)
+json_agent_executor = create_json_agent(
+    llm=OpenAI(temperature=0),
+    toolkit=json_toolkit,
+    verbose=True
+)
+#json_agent_executor.run("How do I Play Crafty+?")
+#agent.run("Tell me about Queen Priestess")
 
-# Character creation interface
-st.set_page_config(layout='wide')
-col1, col2 = st.columns(2)
+#Query
+#query = "What is the stats of Queen Priestess"
+#initial_response = chain({"question": query})
+#print(initial_response)
 
-with col1:
-    st.image(img, caption="A Digital Storytelling Companion")
+# Function to handle user input and generate chatbot response
+# Function to handle user input and generate chatbot response
+def chatbot_response(user_input):
+    # Your chatbot logic goes here
+    cards_question = f"Answer any question regarding the card database: {user_input}"
+    response = agent.run(cards_question)
+    
+    if isinstance(response, dict):
+        try:
+            df = pd.DataFrame.from_dict(response, orient='index', columns=['Value'])
+            df.reset_index(inplace=True)
+            df.rename(columns={'index': 'Key'}, inplace=True)
+            return df
+        except KeyError:
+            return "Sorry, I could not find the information you're looking for."
+    else:
+        return pd.DataFrame({"Key": ["Response"], "Value": [response]})
 
-    st.title("Character Creation")
-    character_name = st.text_input("Enter your character's name:")
-    race_options = ["Human", "Elf", "Dwarf", "Orc", "Tiefling"]
-    character_race = st.selectbox("Select your character's race:", race_options)
-    class_options = ["Warrior", "Mage", "Rogue", "Cleric", "Ranger"]
-    character_class = st.selectbox("Select your character's class:", class_options)
+def chatbot_rules_response(user_input):
+    # Your chatbot logic goes here
+    rules_question = f"Can you give me clear rules on {user_input}"
+    response = json_agent_executor.run(rules_question)
+    
+    if isinstance(response, dict):
+        try:
+            df = pd.DataFrame.from_dict(response, orient='index', columns=['Value'])
+            df.reset_index(inplace=True)
+            df.rename(columns={'index': 'Key'}, inplace=True)
+            return df
+        except KeyError:
+            return "Sorry, I could not find the information you're looking for."
+    else:
+        return pd.DataFrame({"Key": ["Response"], "Value": [response]})
 
-with col2:
-    st.title("Story Generation")
-    if st.button("Submit"):
-        if character_name and character_race and character_class:
-            st.subheader("Character Summary")
-            st.write(f"Name: {character_name}")
-            st.write(f"Race: {character_race}")
-            st.write(f"Class: {character_class}")
-            st.session_state.story_generated = True
 
-    if st.session_state.get("story_generated", False):
-        st.subheader("Story Introduction")
-        story_intro = generate_story_intro(character_name, character_race, character_class)
-        st.write(story_intro)
+# Main function for the Streamlit application
+# Main function for the Streamlit application
+# Main function for the Streamlit application
+def main():
+    # Application title and description
+    st.title("Crafty+ Chatbot")
+    st.write("This is a chatbot that can answer questions about Crafty+.")
 
-        # Generate story and summary
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=f"{story_intro}\n",
-            temperature=0.7,
-            max_tokens=1024,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
-        story = story_intro
-        summary = summarize_story(story)
+    # Set display options for pandas dataframes
+    pd.set_option('display.max_columns', None)  # Show all columns
+    pd.set_option('display.expand_frame_repr', False)  # Don't wrap to multiple pages
+    pd.set_option('display.max_rows', None)  # Show all rows
 
-        # Tweet the story summary
-        tweet_text = f"{summary} #CraftyTales @craftthefuture_ Craft Your Tale: https://craftytales.streamlit.app/"
-        tweet_url = "https://twitter.com/intent/tweet?text=" + urllib.parse.quote(tweet_text)
-        tweet_button = f'<a href="{tweet_url}" target="_blank">Tweet</a>'
-        st.markdown(tweet_button, unsafe_allow_html=True)
+    # Get user input for card information
+    user_input_card = st.text_input("What question do you have about your card?:")
 
-        # Stop execution after the Tweet button is displayed
-        st.stop()
+    # When user presses 'Send' button for card information
+    if st.button("Send Card Info"):
+        # Call the chatbot_response function with user input
+        response = chatbot_response(user_input_card)
 
+        # Display the chatbot's response
+        st.write(f"Chatbot:")
+        st.write(response)
+
+    # Get user input for rules question
+    user_input_rules = st.text_input("Ask a Question about the rules:")
+
+    # When user presses 'Ask' button for rules question
+    if st.button("Ask Rules"):
+        response = chatbot_rules_response(user_input_rules)
+        st.write(f"Chatbot:")
+        st.write(response)
+
+# Run the main function
+if __name__ == "__main__":
+    main()
